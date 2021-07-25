030b21b4d3a99ca347e9bf182e28ee52142231f7
Shane Cavanaugh <shane@shanecav.net>
2021-07-24 22:20:29 -0600

    Make constant available on library load

--- system/social_web/activity_pub.rb
+++ system/social_web/activity_pub.rb
@@ -2,6 +2,10 @@ require 'social_web/activity_pub/container'
 
 module SocialWeb::ActivityPub
   class Error < StandardError; end
+
+  LOG_FORMATTER = lambda do |severity, datetime, progname, msg|
+    ::Logger::Formatter.new.call(severity, datetime, 'SocialWeb::ActivityPub', msg)
+  end
 end
 
 SocialWeb::ActivityPub::Container.start :configuration
--- system/social_web/activity_pub/boot/logging.rb
+++ system/social_web/activity_pub/boot/logging.rb
@@ -6,10 +6,6 @@ SocialWeb::ActivityPub::Container.boot :logging do
 
     require 'logger'
 
-    SocialWeb::ActivityPub::LOG_FORMATTER = lambda do |severity, datetime, progname, msg|
-      ::Logger::Formatter.new.call(severity, datetime, 'SocialWeb::ActivityPub', msg)
-    end
-
     register(
       :logger,
       SocialWeb::ActivityPub.config.logger
